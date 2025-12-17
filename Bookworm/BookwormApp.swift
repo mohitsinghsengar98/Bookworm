f069645805e486da167913656620c8fe96e87a18
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-12-17 15:09:20 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Mohit Sengar on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
